5bba2f7ad37f9efc65987f5e79fa61cb59ea8712
IanGolvach <golvachi@msoe.edu>
2023-03-28 16:30:49 -0500

    Adding testing for multi-letter guesses

--- Test/Feature2Test.java
+++ Test/Feature2Test.java
@@ -11,11 +11,12 @@ public class Feature2Test {
         // neccesary?
     }
 
-    @Test
+
     /**
      * @author Golvachi
      * @version 1.0
      */
+    @Test
     public void greenTest(){
         final int wordLength = 5;
         final String guessString = "truth";
@@ -27,11 +28,12 @@ public class Feature2Test {
         }
     }
 
-    @Test
+
     /**
      * @author Golvachi
      * @version 1.0
      */
+    @Test
     public void grayTest(){
         final int wordLength = 5;
         final String guessString = "mango";
@@ -43,11 +45,12 @@ public class Feature2Test {
         }
     }
 
-    @Test
+
     /**
      * @author Golvachi
      * @version 1.0
      */
+    @Test
     public void yellowTest(){
         final int wordLength = 5;
         final String guessString = "throb";
@@ -57,6 +60,64 @@ public class Feature2Test {
         for(int i = 0; i < wordLength; ++i){
             Assertions.assertEquals(trueArray[i], colorArray[i]);
         }
+    }
+
+    /**
+     * @author golvachi
+     * @version 1.0
+     */
+    @Test
+    public void multiplicities(){
+        final String threeo = "coolio";
+        final String oneo = "creton";
+        final Color[] oArray = {Color.GREEN, Color.YELLOW,
+                Color.GRAY, Color.GRAY, Color.GRAY, Color.GRAY};
+        Color[] colorArray = Wordle.perWordLetterCheck(threeo, oneo);
+        for(int i = 0; i < oArray.length; ++i){
+            Assertions.assertEquals(oArray[i], colorArray[i]);
+        }
+        // Only one of the 'o' in 'coolio' is necessary, and it is not in the correct place.
+        // Therefore, only the first o will be colored, the rest will be gray.
+
+        final String hurry = "hurry";
+        final String rumor = "rumor";
+        final Color[] rArray = {Color.GRAY, Color.GREEN, Color.YELLOW, Color.YELLOW, Color.GRAY};
+        colorArray = Wordle.perWordLetterCheck(hurry, rumor);
+        for(int i = 0; i < rArray.length; ++i){
+            Assertions.assertEquals(rArray[i], colorArray[i]);
+        }
+        // Both rs should be colored yellow, as the first r takes up one possible yellow,
+        // the second r takes the other.
+
+        final String hurri = "hurri";
+        final String rumod = "rumod";
+        final Color[] rArray2 = {Color.GRAY, Color.GREEN, Color.YELLOW, Color.GRAY, Color.GRAY};
+        colorArray = Wordle.perWordLetterCheck(hurry, rumod);
+        for(int i = 0; i < rArray2.length; ++i){
+            Assertions.assertEquals(rArray2[i], colorArray[i]);
+        }
+        // Now that the 'true' word only has one r, the second r should be grayed out
+
+        final String apple = "apple";
+        final String apoge = "apoge";
+        final Color[] pArray = {Color.GREEN, Color.GREEN, Color.GRAY, Color.GRAY, Color.GREEN};
+        colorArray = Wordle.perWordLetterCheck(apple, apoge);
+        for(int i = 0; i < pArray.length; ++i){
+            Assertions.assertEquals(pArray[i], colorArray[i]);
+        }
+        // Because the only p in the truth is already found, the other p is grayed out.
+
+        final String apropos = "apropos";
+        final String applely = "applely";
+        final Color[] pArray2 = {Color.GREEN, Color.GREEN, Color.GRAY, Color.GRAY, Color.YELLOW,
+                                Color.GRAY, Color.GRAY};
+        colorArray = Wordle.perWordLetterCheck(apropos, applely);
+        for(int i = 0; i < pArray2.length; ++i){
+            Assertions.assertEquals(pArray2[i], colorArray[i]);
+        }
+        // There are two 'p' in the truth word, and one correct and one misplaced
+        // The first will be green, and the second will be yellow.
+
     }
 
 
