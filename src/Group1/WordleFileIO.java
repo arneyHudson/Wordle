909039357e134be9fa142d11027d19afc68ae689
IanGolvach <golvachi@msoe.edu>
2023-04-11 15:54:08 -0500

    Made file to hold the I/O methods for User Story 9 and 10

---
+++ src/Group1/WordleFileIO.java
@@ -0,0 +1,23 @@
+
+package Group1;
+
+import java.nio.file.Path;
+import java.util.Dictionary;
+import java.util.HashMap;
+
+/**
+ * Holds all the file input/output methods used by the wordle application.
+ */
+public class WordleFileIO {
+
+
+    /**
+     * Returns a dictionary of each character and its frequency of being guessed in the application
+     * @param characterHistoryPath A text file holding the history of the chars and their frequency
+     * @return A HashMap dictionary of characters and frequency
+     */
+    public HashMap<Character, Integer> loadCharacterFrequency(Path characterHistoryPath){
+        return null;
+    }
+
+}
