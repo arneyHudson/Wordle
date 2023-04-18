49532cceb8e1c681b83cf0200d47bfebd1defe75
IanGolvach <golvachi@msoe.edu>
2023-04-18 16:02:33 -0500

    Added global variables for the character frequency

--- src/Group1/WordleFileIO.java
+++ src/Group1/WordleFileIO.java
@@ -17,6 +17,8 @@ public class WordleFileIO {
     private static final String CHAR_DICT_LINE = "BEGIN_CHAR_DICT";
     private static final String END_LINE = "END";
 
+    public static HashMap<Character, Integer> CHARACTER_FREQUENCY = new HashMap<>();
+    public static Path CHAR_FREQ_PATH = Path.of("ADMIN_FILES/character_frequency_log.txt");
 
     /**
      * Returns a dictionary of each character and its frequency of being guessed in the application
@@ -26,7 +28,7 @@ public class WordleFileIO {
     public static HashMap<Character, Integer> loadCharacterFrequency(Path characterHistoryPath){
         HashMap<Character, Integer> ret = new HashMap<>();
         try(Scanner in = new Scanner(characterHistoryPath)){
-            if(!in.nextLine().equals(CHAR_DICT_LINE)){
+            if(!in.next().equals(CHAR_DICT_LINE)){
                 throw new IOException("Path given is not for the character frequency.");
             }
             String current = in.next();
